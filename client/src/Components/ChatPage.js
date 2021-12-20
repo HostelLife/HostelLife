af0016847e0a69b44674cb4f7f8f631b08756c80
Suman Rayamajhi <meh_sams@yahoo.com>
2021-12-20 23:07:03 +0100

    some change

--- client/src/Components/ChatPage.js
+++ client/src/Components/ChatPage.js
@@ -3,27 +3,7 @@ import CardChat from "./CardChat";
 import { useParams } from "react-router-dom";
 
 function ChatPage() {
-  const [events, setEvents] = useState([]);
-
-  let { id } = useParams();
-
-  useEffect(() => {
-    const url = `http://localhost:5000/events/${id}`;
-
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setEvents(data))
-      .catch((error) => {
-        console.error(error);
-      });
-  }, [id]);
-  return (
-    <div>
-      {events.map((event) => {
-        return <CardChat event={event} />;
-      })}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default ChatPage;
