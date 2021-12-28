0fa18e74477b7ebb86897c0afa7b443bdbf6ad7f
Suman Rayamajhi <meh_sams@yahoo.com>
2021-12-28 14:03:50 +0100

    auto-refresh-eventProfile

--- client/src/Components/EventProfilePage/EventProfilePage.jsx
+++ client/src/Components/EventProfilePage/EventProfilePage.jsx
@@ -6,39 +6,38 @@ export default function EventProfilePage() {
   const [event, setEvent] = useState();
   //getting the localStorage email
   const userInfo = JSON.parse(window.localStorage.getItem("userInfoKey"));
-  const userEmailLocal = userInfo.email;
-  console.log("User mail from local storage " + userEmailLocal);
+  const userEmail = userInfo.email;
+  console.log("User mail from local storage " + userEmail);
   let { id } = useParams();
 
-  // const [searchParams, setSearchParams] = useSearchParams();
-  // searchParams.get("");
-  //?userEmail=${userEmailLocal}
-
   useEffect(() => {
-    const url = `http://localhost:5000/events/${id}?userEmail=${userEmailLocal}`;
-    console.log(userEmailLocal);
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setEvent(data))
-      .catch((error) => {
-        console.error(error);
-      });
-  }, [id, userEmailLocal]);
+    getEventData(id, userEmail).then((eventData) => setEvent(eventData));
+  }, [id, userEmail]);
 
-  const onClick = (event) => {
+  const getEventData = async (eventId, userEmail) => {
+    const response = await fetch(
+      `http://localhost:5000/events/${eventId}?userEmail=${userEmail}`
+    );
+    const jsonResponse = await response.json();
+    return jsonResponse;
+  };
+
+  const onClick = async (event) => {
     event.preventDefault();
 
-    const data = { userEmail: userEmailLocal };
+    const data = { userEmail: userEmail };
 
     const url = `http://localhost:5000/events/${id}/participant`;
-    fetch(url, {
+    await fetch(url, {
       method: "POST",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
-    }).then((res) => console.log(res));
+    });
+    const eventData = await getEventData(id, userEmail);
+    setEvent(eventData);
   };
 
   return <div>{event && <CardsProfile event={event} onClick={onClick} />}</div>;
