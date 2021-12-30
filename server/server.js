28a1e5dacba9a9389ffc9f22eca7ec9f02320fd0
Luxygit <divaro_@hotmail.com>
2021-12-30 17:46:23 +0100

    env done

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const bodyParser = require("body-parser");
